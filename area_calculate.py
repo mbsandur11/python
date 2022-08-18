725d4f2195e8898f0a0165f7d5598b66159486ad
Mahesh Sandur <mbsandur143@gmail.com>
2022-08-18 19:34:12 +0530

    area_calculator

---
+++ area_calculate.py
@@ -0,0 +1,8 @@
+#calculate the area 
+PI = 3.14
+r = float(input(' Please Enter the radius of a circle: '))
+area = PI * r * r
+circumference = 2 * PI * r
+print(" Area Of a Circle = %.2f" %area)
+print(" Circumference Of a Circle = %.2f" %circumference)
+
